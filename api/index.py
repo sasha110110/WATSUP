9240ffacf43052ebcb1be7422fa68b5af2774e96
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-05-26 10:33:14 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,25 @@
 from flask import Flask
+import json
+from flask import request
+import requests
+
+instance_id="instance48357"
+token="c5mz6bh47ltnl6ka"
+url = "https://api.ultramsg.com/instance48357/messages/chat"
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', "GET"])
 def home():
-    return 'Hello, World!'
+    res=""
+    if request.method == 'POST':
+        result=json['data']
+        res+=str(resuult)
+        
 
-@app.route('/about')
-def about():
-    return 'About'
+        payload = f"token={token}&to=%2B79500310422&body=result"
+        payload = payload.encode('utf8').decode('utf8')#('iso-8859-1')
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        response = requests.request("POST", url, data=payload, headers=headers)
+        return ""
+    return res
