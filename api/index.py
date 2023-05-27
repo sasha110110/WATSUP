28e79b6f1206b6dd7337b1066820fb03552ea2a2
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-05-27 19:56:56 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,8 @@ import requests
 
 instance_id="instance48357"
 token="c5mz6bh47ltnl6ka"
-url = "https://api.ultramsg.com/instance48357/messages/chat"
+chatID="79500310422"
+url = f"https://api.ultramsg.com/{instance_id}/messages/chat/"
 
 app = Flask(__name__)
 
@@ -13,15 +14,18 @@ app = Flask(__name__)
 def home():
     res=""
     if request.method == 'POST':
-        msg=request.get_json(force=True)
-        if msg["data"] is not None:
+        msg=request.json
+        if msg["data"] != []:
             
             res+=str(msg['data'])
 
-            payload = f"token={token}&to=%2B79500310422&body=res"
+            payload = f"token={token}&to=%2B{chatID}&body={res}"
             payload = payload.encode('utf8').decode('iso-8859-1')
             headers = {'content-type': 'application/x-www-form-urlencoded'}
+ 
             response = requests.request("POST", url, data=payload, headers=headers)
-            print(response)
+
+            print(response.text)
+
             return "ok"
 
